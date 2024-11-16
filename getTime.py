40497c1f895c5f29dbb472e4c88c31c76edb6e6d
vintagefuture <39503528+vintagefuture@users.noreply.github.com>
2024-11-16 16:18:18 +0000

    Script running on Pi Zero W

--- getTime.py
+++ getTime.py
@@ -1,16 +1,31 @@
-import requests, bs4, time, lxml
+import requests, bs4, time
+from microdotphat import write_string, show, clear
 
 currentTime = int(time.time())
 url = 'http://www.mybustracker.co.uk/?module=consult&mode=busStopQuickSearch&busStopCode=36242849'
 
 while True:
-    res = requests.post(url)
-    res.raise_for_status()
+    clear()
+    
+    try:
+        res = requests.post(url)
+        res.raise_for_status()
 
-    soup = bs4.BeautifulSoup(res.text, features="lxml")
+        soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
-    service_td = soup.find('td', class_='service', string='10')
-    mins_span = service_td.find_next('td', class_='time').find('span', class_='mins')
-    mins_text = mins_span.get_text()
-    print(f'{mins_text} min')
+        service_td = soup.find('td', class_='service', string='10')
+        mins_span = service_td.find_next('td', class_='time').find('span', class_='mins')
+        mins_text = mins_span.get_text()
+        print(f'{mins_text} min')
+        if mins_text != 'DUE':
+            write_string(f'{mins_text} min', kerning=False)
+        else:
+            write_string(mins_text, kerning=False)
+        show()
+    except Exception as e:
+        clear()
+        print("ERROR", e)
+        write_string('ERROR', kerning=False)
+        show()
+    
     time.sleep(30)
